6e45d3185206b296d8e0fe05b1d4d33a4786f5f2
Sahadat20 <shahadathossainroni941@gmail.com>
2025-04-23 17:36:00 +0600

    Client folder added some basic routes

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -4,7 +4,7 @@ import { CustomError } from '../errors/custom-error';
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof CustomError){
         
-        res.status(err.statusCode).send({erros: err.serializeErrors()});
+        res.status(err.statusCode).send({errors: err.serializeErrors()});
         return 
     }
     
